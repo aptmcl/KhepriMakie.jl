0206b0fbffd66d8da20bbc2d97d068a42e62b8a4
Documenter.jl <documenter@juliadocs.github.io>
2022-11-22 19:55:43 +0000

    build based on 0d5b4f2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KhepriMakie","category":"page"},{"location":"#KhepriMakie","page":"Home","title":"KhepriMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KhepriMakie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KhepriMakie]","category":"page"}]
+}
